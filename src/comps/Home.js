bac1caccc7233ba3d3353b3a5686c97fbf7a055f
demma98 <demmmma98@gmail.com>
2023-05-13 21:59:51 -0600

    trans flag

--- src/comps/Home.js
+++ src/comps/Home.js
@@ -6,9 +6,41 @@ const Home = () => {
         <Navbar></Navbar>
         <div className="home">
             <br />
-            im demma
+            <table className="gender">
+            <tbody>
+            <tr>
+                <td className="trans_flag_cell">
+                    <div className="trans_flag">
+                        <div className="trans_flag_blue">
+                            <div className="trans_flag_blank"></div>
+                            <div className="trans_flag_blank"></div>
+                            <div className="trans_flag_blank"></div>
+                            <div className="trans_flag_pink">
+                                <div className="trans_flag_blank"></div>
+                                <div className="trans_flag_blank"></div>
+                                <div className="trans_flag_blank"></div>
+                                <div className="trans_flag_blank"></div>
+                                <div className="trans_flag_white">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </td>
+                <td className="pronouns_cell">
+                    <div className="pronouns">
+                        pronouns : she/her
+                    </div>
+                </td>
+            </tr>
+            </tbody>
+            </table>
             <br />
-            i make stuff
+            <div className="im_demma">
+                im demma
+                <br />
+                i make stuff
+                <br />
+            </div>
         </div>
         </>
     )
